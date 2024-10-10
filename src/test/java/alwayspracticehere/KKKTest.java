ad20acb21863d3e9192bf941d127e9672f3b45b8
pragti gupta <pragtig6@gmail.com>
2024-10-10 13:17:19 +0530

    jk

--- src/test/java/alwayspracticehere/KKKTest.java
+++ src/test/java/alwayspracticehere/KKKTest.java
@@ -8,20 +8,25 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Clickmultiways111Test {
+public class KKKTest {
+	
 	
 	WebDriver driver;
 	
+	
   @Test
-  public void fTest() throws InterruptedException {    
+  public void fTest() {
 	  
+	 
+	  
+  
 	  
 	  driver=new ChromeDriver();
 	  driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-		driver.get("https://www.globalsqa.com/demo-site/");
+		driver.get("https://www.makemytrip.com/");
 		
 	
 		driver.findElement(By.xpath("//li[@id='menu-item-51137']//a[normalize-space()='Home']")).sendKeys(Keys.RETURN);
@@ -29,7 +34,9 @@ public class Clickmultiways111Test {
 	
 		driver.findElement(By.xpath("//li[@id='menu-item-6898']//a[normalize-space()='CheatSheets']")).sendKeys(Keys.ENTER);
 		
-		
+String sr=driver.getWindowHandle();
+
+System.out.println(sr);
 		
 		
 		
